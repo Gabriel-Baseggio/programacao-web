5e8cf96abded1c8ceab167ca282554afc91fd1cf
Gabriel-Baseggio <baseggiogabriel6@gmail.com>
2023-06-13 21:44:17 -0300

    Update

--- javascript/DOM2/atv1/js/script.js
+++ javascript/DOM2/atv1/js/script.js
@@ -9,27 +9,3 @@ botaoAumentar.addEventListener("click", () => {
 botaoDiminuir.addEventListener("click", () => {
     contador.innerHTML = parseInt(contador.innerHTML) - 1;
 });
-
-var id;
-botaoAumentar.onmousedown = () => {
-    contador.innerHTML = parseInt(contador.innerHTML) + 1;
-
-    id = setInterval(() => {
-        contador.innerHTML = parseInt(contador.innerHTML) + 1;
-    }, 1)
-}
-botaoAumentar.onmouseup = () => {
-    clearInterval(id)
-}
-
-var id2;
-botaoDiminuir.onmousedown = () => {
-    contador.innerHTML = parseInt(contador.innerHTML) - 1;
-
-    id2 = setInterval(() => {
-        contador.innerHTML = parseInt(contador.innerHTML) - 1;
-    }, 1)
-}
-botaoDiminuir.onmouseup = () => {
-    clearInterval(id2)
-}
