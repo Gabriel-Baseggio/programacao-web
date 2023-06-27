5fdc4bb6f19408ed6d30e0e8c70ee1c31f2d90cf
Gabriel-Baseggio <baseggiogabriel6@gmail.com>
2023-06-27 09:07:42 -0300

    l

--- javascript/webStorage/quiz/js/script.js
+++ javascript/webStorage/quiz/js/script.js
@@ -6,51 +6,55 @@ document.getElementById("botao-comecar").addEventListener('click', () => {
     let jogador = "";
     do {
         jogador = prompt("Digite seu nome");
-
+        
         if (!jogador) {
             alert("Digite um nome válido!")
         }
     } while(!jogador);
     
-    document.body.innerHTML = "";
+    let q = ["Quanto tempo você tem para usar o dash da Jett após ativá-lo?", "", "", ""];
+    let opcoes = [["14 segundos", "8 segundos", "10 segundos", "12 segundos"], [], [], []];
     let pontos = 0;
-    let q = ["Quanto tempo você tem para usar o dash da Jett após ativá-lo?"];
-    let opcoes = [["12 segundos", "8 segundos", "10 segundos", "14 segundos"], [], []];
-    
-    for (let i = 0; i < 1; i++) {
-        console.log("XD");
-        questao(q[i], opcoes[i]);
-        console.log("XD2");
-    }
+
+    pontos += mostrarQuestao(q, opcoes, 0);
     
     localStorage.setItem("ranking", JSON.stringify([jogador, pontos]));
     
 });
 
-function questao(questaoTxt, opcoes) {
+function mostrarQuestao(q, opcoes, pontos) {
+    if (q.length <= 0 || opcoes.length <= 0) {
+        return pontos;
+    }
+    document.body.innerHTML = "";
     let questao = document.createElement("h1");
-    questao.textContent = questaoTxt; 
+    questao.textContent = q[0]; 
     let div = document.createElement("div"); 
+    let opc = opcoes[0];
 
     for (let i = 0; i < 4; i++) {
         let resp = document.createElement("button");
-
-        resp.textContent = opcoes[i];
-        resp.addEventListener('click', () => {
-            
-        })
-
+        
+        resp.textContent = opc[i];
+        if (i == 3) {
+            resp.addEventListener('click', () => {
+                resp.style.backgroundColor = "green";
+                q.unshift();
+                opcoes.unshift();
+                pontos++;
+                mostrarQuestao(q, opcoes, pontos);
+            })
+        } else {
+            resp.addEventListener('click', () => {
+                resp.style.backgroundColor = "red";
+                q.unshift();
+                opcoes.unshift();
+                mostrarQuestao(q, opcoes, pontos);
+            })
+        }
         div.appendChild(resp);
     }
 
     document.body.appendChild(questao);
     document.body.appendChild(div);
-}
-
-function errou(el) {
-    el.style.backgroundColor = "red";
-}
-
-function acertou(el) {
-    el.style.backgroundColor = "green";
 }
